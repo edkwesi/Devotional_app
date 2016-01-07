f6e5944b59e2045bd8be3a889e781832b55663a6
supasheva <kwadwobusumtwi@hotmail.com>
2016-01-07 13:18:55 +0000

    Made changes to controller singleDevotion

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -8,14 +8,23 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('devotionListCtrl', function($scope,devotionService) {
+.controller('devotionListCtrl', function($scope,devotionService,$location) {
 	$scope.devotions= devotionService.all;
+
+	$scope.singleDev=function(id){
+	$location.path("/Dev/"+id);
+
+	}
+
+
 })
 
 .controller('singleDevCtrl', function($scope,devotionService,$stateParams,$state) {
-	$scope.singleDevotion = deovtionService.get($stateParams.datee);
-    $scope.ingList =  $scope.singleDevotion.content;
-    $scope.prepList = $scope.singleDevotion.text;
+	$scope.singleDevotion =devotionService.get($stateParams.id);
+    $scope.content =  $scope.singleDevotion.content;
+    $scope.date = $scope.singleDevotion.datee;
+    $scope.text = $scope.singleDevotion.text;
+    $scope.quote = $scope.singleDevotion.quote;
 })
    
 .controller('podcastsCtrl', function($scope) {
